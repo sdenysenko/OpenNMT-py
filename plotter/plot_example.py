70d2bf060851eed5c093bf2afe17f4475e889e97
Alexandr <denysenkooo@gmail.com>
2019-03-01 16:27:17 +0000

    fix plotter, now it works right

--- plotter/plot_example.py
+++ plotter/plot_example.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 
-def save_plot(data, original_text, prediction_text, number):
+def save_plot(data, src_list, pred_list, number):
 
-	plt.pcolormesh(data)
-	plt.ylabel(prediction_text)
-	plt.xlabel(original_text)
-	plt.plot()
-	plt.savefig('/home/user/ClonedRepo/OpenNMT-py/plotter/data/spectr' + number + '.png')
+	fig, ax = plt.subplots(figsize=(int(len(src_list) * 1.2 + 1), int(len(pred_list) + 1)))
+	data = data[:len(pred_list)]
+	for i in range(len(data)):
+		data[i] = data[i][:len(src_list)]
+	im = ax.imshow(data, aspect='auto', origin='lower', interpolation='none', vmin=0, vmax=1)
+	fig.colorbar(im, ax=ax)
+	xlabel = 'Source sentence'
+	plt.gca().invert_yaxis()
+	plt.xlabel(xlabel)
+	plt.ylabel('Prediction sentence')
+	plt.xticks(range(len(src_list)), src_list, rotation=-10)
+	plt.yticks(range(len(pred_list)), pred_list)
+	plt.tight_layout(pad=1)
+	plt.rcParams['font.size'] = 12
+	plt.savefig('/home/user/ClonedRepo/OpenNMT-py/plotter/data/' + str(number) + '.png')
